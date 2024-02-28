68ca61867268afe9e51bed0a00a89526f8d43d29
kosperera <therealkosala@gmail.com>
2024-02-28 08:29:13 +0000

    style: use prettier for document formatting
    - configure prettier side-by-side with eslint
    - use prettier to format code
    - use editor config for to override default js specific rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
-import js from "@eslint/js";
+import js from '@eslint/js';
 import jsdoc from 'eslint-plugin-jsdoc';
+import prettier from 'eslint-plugin-prettier/recommended';
 
 export default [
   js.configs.recommended,
   jsdoc.configs['flat/recommended'],
+  prettier,
   {
     plugins: { jsdoc },
-    rules: {}
-  }
+    rules: {},
+  },
 ];
